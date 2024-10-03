a19274f7b9758e04e7dd54f41895946a2e1676c1
GOCKHAM <ssayar482@gmail.com>
2024-10-03 16:44:03 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,82 +1,129 @@
+#include <Arduino.h>
+#include <Adafruit_Sensor.h>
+/*
+  Rui Santos & Sara Santos - Random Nerd Tutorials
+  Complete project details at https://RandomNerdTutorials.com/esp32-web-bluetooth/
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
 
-const int ledPin = 2; // Pas dit aan naar je GPIO pin
+BLEServer* pServer = NULL;
+BLECharacteristic* pSensorCharacteristic = NULL;
+BLECharacteristic* pLedCharacteristic = NULL;
 bool deviceConnected = false;
+bool oldDeviceConnected = false;
+uint32_t value = 0;
 
-// BLE Service en Characteristic UUIDs
-#define SERVICE_UUID "064fd7fc-23b7-4518-840d-d5a475223b3b"
+const int ledPin = 26; // Use the appropriate GPIO pin for your setup
+
+#define SERVICE_UUID        "064fd7fc-23b7-4518-840d-d5a475223b3b"
+#define SENSOR_CHARACTERISTIC_UUID "0c1a1fdf-b5a9-4f24-913d-d289fea37475"
 #define LED_CHARACTERISTIC_UUID "e97eeab2-0d47-4ce3-8590-959073d5b2c2"
 
-// Callback class om te reageren op schrijfacties
-class MyCharacteristicCallbacks : public BLECharacteristicCallbacks {
-  void onWrite(BLECharacteristic* pLedCharacteristic) {
-    std::string ledvalue = pLedCharacteristic->getValue();
-    Serial.print("Received value: ");
-    Serial.println(ledvalue.c_str());
-
-    if (ledvalue == "ON") {
-      digitalWrite(ledPin, HIGH);  // Zet LED aan
-      Serial.println("LED ON");
-    } else if (ledvalue == "OFF") {
-      digitalWrite(ledPin, LOW);   // Zet LED uit
-      Serial.println("LED OFF");
-    } else {
-      Serial.println("Unknown command received");
-    }
-  }
-};
-
-class MyServerCallbacks : public BLEServerCallbacks {
+class MyServerCallbacks: public BLEServerCallbacks {
   void onConnect(BLEServer* pServer) {
     deviceConnected = true;
-    Serial.println("Device Connected");
-  }
+  };
 
   void onDisconnect(BLEServer* pServer) {
     deviceConnected = false;
-    Serial.println("Device Disconnected");
+  }
+};
+
+class MyCharacteristicCallbacks : public BLECharacteristicCallbacks {
+  void onWrite(BLECharacteristic* pLedCharacteristic) {
+    std::string value = pLedCharacteristic->getValue(); // Gebruik std::string hier
+    if (value.length() > 0) {
+      Serial.print("Characteristic event, written: ");
+      Serial.println(value.c_str()); // Print de geschreven waarde
+
+      // Controleer op "ON" of "OFF" om de LED te bedienen
+      if (value == "ON") {
+        digitalWrite(ledPin, HIGH); // Zet de LED aan
+        Serial.println("LED is ON");
+      } else if (value == "OFF") {
+        digitalWrite(ledPin, LOW); // Zet de LED uit
+        Serial.println("LED is OFF");
+      }
+    }
   }
 };
 
 void setup() {
   Serial.begin(115200);
   pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, LOW); // Zorg dat de LED uit is in het begin
 
-  BLEDevice::init("ESP32SINS");
-  BLEServer *pServer = BLEDevice::createServer();
+  // Create the BLE Device
+  BLEDevice::init("ESP32");
+
+  // Create the BLE Server
+  pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
 
+  // Create the BLE Service
   BLEService *pService = pServer->createService(SERVICE_UUID);
-  BLECharacteristic *pLedCharacteristic = pService->createCharacteristic(
-      LED_CHARACTERISTIC_UUID,
-      BLECharacteristic::PROPERTY_WRITE
-  );
 
+  // Create a BLE Characteristic
+  pSensorCharacteristic = pService->createCharacteristic(
+                      SENSOR_CHARACTERISTIC_UUID,
+                      BLECharacteristic::PROPERTY_READ   |
+                      BLECharacteristic::PROPERTY_WRITE  |
+                      BLECharacteristic::PROPERTY_NOTIFY |
+                      BLECharacteristic::PROPERTY_INDICATE
+                    );
+
+  // Create the LED control Characteristic
+  pLedCharacteristic = pService->createCharacteristic(
+                      LED_CHARACTERISTIC_UUID,
+                      BLECharacteristic::PROPERTY_WRITE
+                    );
+
+  // Register the callback for the LED control characteristic
   pLedCharacteristic->setCallbacks(new MyCharacteristicCallbacks());
+
+  // Add descriptors for the characteristics
+  pSensorCharacteristic->addDescriptor(new BLE2902());
+  pLedCharacteristic->addDescriptor(new BLE2902());
+
+  // Start the service
   pService->start();
-  pServer->getAdvertising()->start();
-  Serial.println("BLE Server is now advertising.");
+
+  // Start advertising
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(false);
+  pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
+  BLEDevice::startAdvertising();
+  Serial.println("Waiting a client connection to notify...");
 }
 
 void loop() {
-  static unsigned long lastPrintTime = 0; // Houd de tijd bij voor het afdrukken van meldingen
-  unsigned long currentMillis = millis(); // Huidige tijd in milliseconden
-
-  // Controleer regelmatig de verbinding
-  if (currentMillis - lastPrintTime >= 2000) { // Elke 2 seconden
-    if (deviceConnected) {
-      Serial.println("Device is connected.");
-    } else {
-      Serial.println("Device is not connected.");
-    }
-    lastPrintTime = currentMillis; // Reset de tijd
+  // Notify changed value
+  if (deviceConnected) {
+    pSensorCharacteristic->setValue(String(value).c_str());
+    pSensorCharacteristic->notify();
+    value++;
+    Serial.print("New value notified: ");
+    Serial.println(value);
+    delay(3000); // avoid congestion in the bluetooth stack
   }
-
-  // Voer hier je andere code uit, zoals lezen van sensoren, etc.
-  delay(100); // Voeg een korte vertraging toe om de CPU te ontlasten
+  
+  // Disconnecting
+  if (!deviceConnected && oldDeviceConnected) {
+    Serial.println("Device disconnected.");
+    delay(500); // give the bluetooth stack the chance to get things ready
+    pServer->startAdvertising(); // restart advertising
+    Serial.println("Start advertising");
+    oldDeviceConnected = deviceConnected;
+  }
+  
+  // Connecting
+  if (deviceConnected && !oldDeviceConnected) {
+    oldDeviceConnected = deviceConnected;
+    Serial.println("Device Connected");
+  }
 }
-
