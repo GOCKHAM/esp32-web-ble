af29b3f2d62bd7201318fc53511690931dd4a97b
GOCKHAM <ssayar482@gmail.com>
2024-10-08 10:08:07 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,14 +7,19 @@
 #include <BLE2902.h>
 
 BLEServer* pServer = NULL;
+BLECharacteristic* pSensorCharacteristic = NULL;
 BLECharacteristic* pLedCharacteristic = NULL;
 bool deviceConnected = false;
 bool oldDeviceConnected = false;
+uint32_t value = 0;
 
 static const int servoPin = D3;
 Servo servo1; 
 
-#define SERVICE_UUID "19b10000-e8f2-537e-4f6c-d104768a1214"
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+#define SERVICE_UUID        "19b10000-e8f2-537e-4f6c-d104768a1214"
+#define SENSOR_CHARACTERISTIC_UUID "19b10001-e8f2-537e-4f6c-d104768a1214"
 #define LED_CHARACTERISTIC_UUID "19b10002-e8f2-537e-4f6c-d104768a1214"
 
 class MyServerCallbacks: public BLEServerCallbacks {
@@ -29,7 +34,7 @@ class MyServerCallbacks: public BLEServerCallbacks {
 
 class MyCharacteristicCallbacks : public BLECharacteristicCallbacks {
   void onWrite(BLECharacteristic* pLedCharacteristic) {
-    std::string ledvalue = pLedCharacteristic->getValue(); 
+    std::string ledvalue  = pLedCharacteristic->getValue(); 
     String value = String(ledvalue.c_str());
     if (value.length() >= 0) {
       Serial.print("Characteristic event, written: ");
@@ -37,11 +42,11 @@ class MyCharacteristicCallbacks : public BLECharacteristicCallbacks {
 
       int receivedValue = static_cast<int>(value[0]);
       if (receivedValue == 1) {
-        servo1.write(65); // Move servo to open position
-        delay(20); // Small delay for servo to move
+        servo1.write(65);
+        delay(20);
       } else {
-        servo1.write(0); // Move servo to closed position
-        delay(20); // Small delay for servo to move
+        servo1.write(0);
+        delay(20);
       }
     }
   }
@@ -49,10 +54,10 @@ class MyCharacteristicCallbacks : public BLECharacteristicCallbacks {
 
 void setup() {
   Serial.begin(115200);
-  servo1.attach(servoPin); // Attach servo to pin
+  servo1.attach(servoPin);
 
   // Create the BLE Device
-  BLEDevice::init("ESP32SINS");
+  BLEDevice::init("ESP32SINE");
 
   // Create the BLE Server
   pServer = BLEDevice::createServer();
@@ -61,16 +66,27 @@ void setup() {
   // Create the BLE Service
   BLEService *pService = pServer->createService(SERVICE_UUID);
 
-  // Create the ON/OFF button Characteristic
+  // Create a BLE Characteristic
+  pSensorCharacteristic = pService->createCharacteristic(
+                      SENSOR_CHARACTERISTIC_UUID,
+                      BLECharacteristic::PROPERTY_READ   |
+                      BLECharacteristic::PROPERTY_WRITE  |
+                      BLECharacteristic::PROPERTY_NOTIFY |
+                      BLECharacteristic::PROPERTY_INDICATE
+                    );
+
+  // Create the ON button Characteristic
   pLedCharacteristic = pService->createCharacteristic(
                       LED_CHARACTERISTIC_UUID,
                       BLECharacteristic::PROPERTY_WRITE
                     );
 
-  // Register the callback for the ON/OFF button characteristic
+  // Register the callback for the ON button characteristic
   pLedCharacteristic->setCallbacks(new MyCharacteristicCallbacks());
 
-  // Add a BLE Descriptor for the characteristic
+  // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
+  // Create a BLE Descriptor
+  pSensorCharacteristic->addDescriptor(new BLE2902());
   pLedCharacteristic->addDescriptor(new BLE2902());
 
   // Start the service
@@ -80,23 +96,32 @@ void setup() {
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
   pAdvertising->setScanResponse(false);
-  pAdvertising->setMinPreferred(0x0);  // Set value to 0x00 to not advertise this parameter
+  pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
   BLEDevice::startAdvertising();
-  Serial.println("Waiting for a client connection to notify...");
+  Serial.println("Waiting a client connection to notify...");
 }
 
 void loop() {
-  // Disconnecting
+  // notify changed value
+  if (deviceConnected) {
+    pSensorCharacteristic->setValue(String(value).c_str());
+    pSensorCharacteristic->notify();
+    value++;
+    //Serial.print("New value notified: ");
+    //Serial.println(value);
+    delay(3000); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
+  }
+  // disconnecting
   if (!deviceConnected && oldDeviceConnected) {
     Serial.println("Device disconnected.");
-    delay(500); // Give the Bluetooth stack time to get ready
-    pServer->startAdvertising(); // Restart advertising
+    delay(500); // give the bluetooth stack the chance to get things ready
+    pServer->startAdvertising(); // restart advertising
     Serial.println("Start advertising");
     oldDeviceConnected = deviceConnected;
   }
-  // Connecting
+  // connecting
   if (deviceConnected && !oldDeviceConnected) {
-    // Do stuff here on connecting
+    // do stuff here on connecting
     oldDeviceConnected = deviceConnected;
     Serial.println("Device Connected");
   }
