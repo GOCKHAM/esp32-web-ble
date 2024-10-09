75660132a4a9222aec7967ca2d3cb89812376a49
GOCKHAM <ssayar482@gmail.com>
2024-10-09 19:58:49 +0200

    credentialss.h

---
+++ credentialss.h
@@ -0,0 +1,6 @@
+// Wi-Fi-netwerk gegevens
+
+#define WIFI_SSID "WiFi-2.4-B070_EXT"       // Vervang door jouw WiFi SSID
+#define WIFI_PASSWORD "SC0498401980"  // Vervang door jouw WiFi-wachtwoord
+
+
